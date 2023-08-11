af7c06d8e06358549130d283cb3507bb6f74548e
RANDRIANAINA Georges Aaron <georges-aaron.randrianaina@irisa.fr>
2023-08-11 18:08:17 +0200

    Fanazavana kely mombamomba fanokonana ny Sikidy

---
+++ sikidy.lisp
@@ -0,0 +1,33 @@
+;;
+;;                 SIKIDY
+;;
+;;              p4    p3    p2    p1
+;;
+;;              •    • •    •     •    p5              ┐
+;;              •    • •    •     •    p6              │ Renin-tsikidy
+;;             • •   • •   • •    •    p7              │
+;;             • •    •    • •    •    p8              ┘
+;;
+;; • •   • •   • •    •     •     •    • •   • •       ┐
+;; • •   • •   • •    •     •     •    • •   • •       │ Zanan-tsikidy
+;;  •     •    • •   • •   • •    •     •     •        │
+;; • •   • •   • •   • •    •    • •    •     •        ┘
+;; p9    p10   p11   p12   p13   p14   p15   p16
+;; -------------------------------------
+;; p1			-- tale
+;; p2			-- maly
+;; p3			-- fahatelo
+;; p4			-- bilady
+;; p5			-- fianahana
+;; p6			-- abily
+;; p7			-- alisay
+;; p8			-- fahavalo
+;; p9	(p7 + p8)	-- fahasivy
+;; p10	(p9 + p11) 	-- ombiasy
+;; p11	(p5 + p6)	-- haja
+;; p12	(p10 + p14)	-- haky
+;; p13	(p3 + p4)	-- asorità
+;; p14	(p13 + p15)	-- saily
+;; p15	(p1 + p2)	-- safary
+;; p16	(p12 + p1)	-- kiba
+;; -------------------------------------
